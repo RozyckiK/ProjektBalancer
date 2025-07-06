19be96fbf5b7f4484d301efdc41cf5ac0e549777
RozyckiK <krozycki7@gmail.com>
2025-07-06 18:20:44 +0200

    Added working ansible playbooks to terraform infrastructure

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,9 +8,9 @@ terraform {
 }
 
 provider "proxmox" {
-    pm_api_url          = "https://YOUR_IP/api2/json"            #Do wypełnienia i opisania
-    pm_api_token_id     = "TOKEN ID"                             #Do wypełnienia i opisania
-    pm_api_token_secret = "API TOKEN SECRET"                     #Do wypełnienia i opisania
+    pm_api_url          = "https://YOUR_IP/api2/json"               #Do wypełnienia i opisania
+    pm_api_token_id     = "TOKEN_ID"                                #Do wypełnienia i opisania
+    pm_api_token_secret = "TOKEN_SECRET"                            #Do wypełnienia i opisania
     pm_tls_insecure     = true
 
     /*
@@ -28,15 +28,17 @@ resource "proxmox_vm_qemu" "vm-instance" {
     count               = 3
     name                = "vm-instance-${count.index + 1}"
     target_node         = "pvetest"
-    clone               = "tmpl"                                            #Do wypełnienia i opisania
+    clone               = "tmpl2"                                            #Do wypełnienia i opisania
     full_clone          = true
     cores               = 2
     memory              = 2048
     vmid                = "${count.index + 6600 + 1}"
+    agent = 1
+    additional_wait     = 60
 
     disk {
         slot            = "scsi0"
-        size            = "10G"
+        size            = "32G"                                             #Do wypełnienia i opisania
         storage         = "local-lvm"                                       #Do wypełnienia i opisania
     }
 
@@ -47,5 +49,40 @@ resource "proxmox_vm_qemu" "vm-instance" {
         firewall  = false
         link_down = false
     }
+}
 
+resource "local_file" "ansible_inventory" {
+  content = <<EOT
+[masters]
+${proxmox_vm_qemu.vm-instance[0].name} ansible_host=${proxmox_vm_qemu.vm-instance[0].default_ipv4_address} ansible_user=root ansible_ssh_private_key_file=~/.ssh/id_ed25519 ansible_ssh_common_args='-o StrictHostKeyChecking=no'
+
+[workers]
+%{ for idx, vm in proxmox_vm_qemu.vm-instance }
+%{ if idx > 0 }
+${vm.name} ansible_host=${vm.default_ipv4_address} ansible_user=root ansible_ssh_private_key_file=~/.ssh/id_ed25519 ansible_ssh_common_args='-o StrictHostKeyChecking=no'
+%{ endif }
+%{ endfor }
+EOT
+
+  filename = "${path.module}/hosts"
+}
+
+resource "null_resource" "ansible_provision" {
+  # Zależność: odpali się dopiero po VM i pliku hosts!
+  depends_on = [proxmox_vm_qemu.vm-instance, local_file.ansible_inventory]
+
+  # Komenda odpala ansible-playbook na host-maszynie (twoim controllerze)
+  provisioner "local-exec" {
+    command = <<EOT
+      sleep 30
+      ansible-playbook -i ${path.module}/hosts playbooks/fix_dns.yml
+      ansible-playbook -i ${path.module}/hosts playbooks/host_name_change.yml
+      ansible-playbook -i ${path.module}/hosts playbooks/k8s_prepare.yml
+      ansible-playbook -i ${path.module}/hosts playbooks/k8s_containerd.yml
+      ansible-playbook -i ${path.module}/hosts playbooks/k8s_tools.yml
+      ansible-playbook -i ${path.module}/hosts playbooks/master_init.yml
+      ansible-playbook -i ${path.module}/hosts playbooks/worker_join.yml
+      ansible-playbook -i ${path.module}/hosts playbooks/k8s_deploy.yml
+    EOT
+  }
 }
